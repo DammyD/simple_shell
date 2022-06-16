9b2e85e96e84f188197bf174b7a4086430017774
DammyD <ibukunoluwadawodu@gmail.com>
2022-06-15 17:33:50 -0700

    Update main.h

--- main.h
+++ main.h
@@ -8,10 +8,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <erron.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <limits.h>
 
 #define BUFFER 1024
 #define TRUE 1
